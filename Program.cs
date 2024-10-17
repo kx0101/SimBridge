df5bb55e7490a757c5bbc6cd4c79a78f735fe4fb
kx0101 <liakos.koulaxis@yahoo.com>
2024-10-17 09:07:22 +0300

    refactor

--- Program.cs
+++ Program.cs
@@ -2,60 +2,51 @@
 
 namespace DynamicModuleSystem
 {
-    public class PanelInfo
+    public class Panel : IHardwareModule
     {
-        public String Name { get; }
-        public List<Oid> Oids { get; }
-        public String Port { get; }
+        protected SerialPortManager _communication = new SerialPortManager();
+
+        protected string serialData;
+        protected string previousData;
+
+        protected Oid oidTarget = Oid.Undefined;
+        protected List<Oid> Oids = new List<Oid>();
+
+        public string Name;
+        public string Port { get; }
         public bool Status { get; }
 
-        public PanelInfo(String name, List<Oid> oids, String port, bool status)
+        public override String ToString()
+        {
+            return "Name: " + Name + " Port: " + Port + " Status: " + Status;
+        }
+
+        public Panel(string name, List<Oid> oids, string port, bool status)
         {
             Name = name;
             Oids = oids;
             Port = port;
             Status = status;
         }
-    }
-
-    public class BaseModule : IHardwareModule
-    {
-        protected SerialPortManager _communication = new SerialPortManager();
-        protected string serialData;
-        protected string previousData;
-        protected Oid oidTarget = Oid.Undefined;
-        private readonly Dictionary<string, PanelInfo> _panels = new Dictionary<string, PanelInfo>();
-
-        protected void Log(string message)
-        {
-            Console.WriteLine(message);
-        }
 
         public virtual bool Initialize(IConfig config)
         {
+            Console.WriteLine($"Initializing panel {Name}");
             return true;
         }
 
         public virtual bool Connect(IConfig config)
         {
+            Console.WriteLine($"Connecting panel {Name}");
+            _communication?.ScanForSerialPorts();
+            _communication?.OpenPort(Port);
+            _communication.DataReceived += (portName, data) => { serialData = data; };
+
             return true;
         }
 
         public virtual void Disconnect()
         {
-        }
-
-        protected void RegisterPanel(Panel panel)
-        {
-            if (!_panels.ContainsKey(panel.Name))
-            {
-                var panelInfo = new PanelInfo(panel.Name, panel.Oids, panel.Port, panel.Status);
-                _panels[panel.Name] = panelInfo;
-            }
-            else
-            {
-                throw new ArgumentException($"Panel '{panel.Name}' is already registered");
-            }
         }
 
         public void OnDataReceived(String panelName, string data)
@@ -85,17 +76,12 @@ namespace DynamicModuleSystem
             }
 
             ParsedData parsed = new ParsedData(parts[0], parts[1], parts[2]);
-            foreach (var panelEntry in _panels)
+            foreach (var currOid in Oids)
             {
-                var currOids = panelEntry.Value.Oids;
-
-                foreach (var currOid in currOids)
+                if (currOid.ToString() == parsed.Link)
                 {
-                    if (currOid.ToString() == parsed.Link)
-                    {
-                        oidTarget = currOid;
-                        break;
-                    }
+                    oidTarget = currOid;
+                    break;
                 }
             }
 
@@ -154,89 +140,10 @@ namespace DynamicModuleSystem
             previousData = serialData;
             return true;
         }
-    }
 
-    public class Module : BaseModule
-    {
-        public string Name { get; private set; }
-        public List<Panel> Panels { get; private set; }
-
-        public Module(string name)
+        protected void Log(string message)
         {
-            Name = name;
-            Panels = new List<Panel>();
-        }
-
-        public void AddPanel(Panel panel)
-        {
-            Panels.Add(panel);
-            RegisterPanel(panel);
-        }
-
-        public void InitializeAll(IConfig config)
-        {
-            Initialize(config);
-            foreach (var panel in Panels)
-            {
-                panel.Initialize(config);
-            }
-        }
-
-        public void ConnectAll(IConfig config)
-        {
-            Connect(config);
-            foreach (var panel in Panels)
-            {
-                panel.Connect(config);
-            }
-        }
-
-        public void DisconnectAll()
-        {
-            Disconnect();
-            foreach (var panel in Panels)
-            {
-                panel.Disconnect();
-            }
-        }
-    }
-
-    public class Panel : BaseModule
-    {
-        public string Name { get; private set; }
-        public List<string> OidsStrings { get; private set; }
-        public List<Oid> Oids = new List<Oid>();
-        public string Port { get; private set; }
-        public bool Status { get; private set; }
-
-        public Panel(string name, List<string> oids, string port, bool status)
-        {
-            Name = name;
-            OidsStrings = oids;
-            Port = port;
-            Status = status;
-
-            foreach (var oid in OidsStrings)
-            {
-                if (Enum.TryParse(oid, out Oid parsedOid))
-                {
-                    if (Oids.Contains(parsedOid))
-                    {
-                        throw new ArgumentException($"OID '{parsedOid}' is already assigned to this panel '{Name}'. OIDs must be unique within each panel.");
-                    }
-
-                    Oids.Add(parsedOid);
-                }
-                else
-                {
-                    Log($"Warning: OID '{oid}' could not be parsed into Oid");
-                }
-            }
-        }
-
-        public override string ToString()
-        {
-            return $"{Name} on {Port} with OIDs: {string.Join(", ", Oids)}";
+            Console.WriteLine(message);
         }
     }
 
@@ -246,10 +153,11 @@ namespace DynamicModuleSystem
         {
             var json = File.ReadAllText("./Config/oids.json");
             var modulesConfig = JsonConvert.DeserializeObject<ModulesConfig>(json);
+            List<Panel> panels = new List<Panel>();
 
             foreach (var moduleEntry in modulesConfig.Modules)
             {
-                var module = new Module(moduleEntry.Key);
+                var parentPanelName = moduleEntry.Key;
                 foreach (var panelEntry in moduleEntry.Value)
                 {
                     var panelName = panelEntry.Key;
@@ -257,28 +165,31 @@ namespace DynamicModuleSystem
                     var port = panelEntry.Value.Port;
                     var status = panelEntry.Value.Status;
 
-                    var panel = new Panel(module.Name + "." + panelName, oids, port, status);
-                    module.AddPanel(panel);
-                }
-
-                module.InitializeAll(new Config());
-                module.ConnectAll(new Config());
-
-                foreach (var panel in module.Panels)
-                {
-                    if (!panel.Status)
+                    if (!status)
                     {
-                        Console.WriteLine($"{panel.Name} is not enabled");
+                        Console.WriteLine($"{panelName} is not enabled");
                         continue;
                     }
 
+                    var oidEnumList = oids.Select(oid => Enum.TryParse<Oid>(oid, out var parsedOid) ? parsedOid : Oid.Undefined).ToList();
+
+                    var panel = new Panel(parentPanelName + "." + panelName, oidEnumList, port, status);
+                    panels.Add(panel);
+                }
+
+                foreach (var panel in panels)
+                {
+                    Console.WriteLine("panel: " + panel);
+                    panel.Initialize(new Config());
+                    panel.Connect(new Config());
+
                     if (panel.Name == "Pedestal.Trim")
                     {
-                        module.OnDataReceived(panel.Name, "OverheadBrightForOledStep I 100");
+                        panel.OnDataReceived(panel.Name, "OverheadBrightForOledStep I 100");
                     }
                     else if (panel.Name == "Pedestal.Takis")
                     {
-                        module.OnDataReceived(panel.Name, "oid1 F 12.5");
+                        panel.OnDataReceived(panel.Name, "oid1 F 12.5");
                     }
                 }
 
@@ -347,6 +258,23 @@ namespace DynamicModuleSystem
 
     public class SerialPortManager
     {
+        public event Action<string, string> DataReceived;
+
+        private void OnDataReceived(object sender, EventArgs e)
+        {
+            Console.WriteLine("subscribed to OnDataReceived");
+        }
+
+        public void OpenPort(string port)
+        {
+            Console.WriteLine($"Opening port {port}...");
+        }
+
+        public void ScanForSerialPorts()
+        {
+            Console.WriteLine("Scanning for ports...");
+        }
+
         public void WriteToPort(string text)
         {
             Console.WriteLine("Writing to port...: " + text);
@@ -364,6 +292,5 @@ namespace DynamicModuleSystem
 
     public interface IConfig
     {
-
     }
 }
